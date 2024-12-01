cbf6358fe48ac9d586acc1dc2b6260e0add368d4
pelumi adeyemi <pelumiadeyemi111@gmail.com>
2024-12-01 19:13:01 +0100

    .

--- src/components/molecules/aboutUs/index.tsx
+++ src/components/molecules/aboutUs/index.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-// import { useScreenResolution } from "@/lib/extentions/hook/useScreenResolution";
-// import Partner from "../../atom/partners";
-// import Testimonials from "@/components/atoms/testimonial";
+
 import { AboutPage } from "./component/heroSection";
 import { CarouselWithModal } from "./component/carouselWithModal";
-// import { BookFlipComponent } from "@/components/atoms/bookSlide";
-
-
-// import AboutTeam from "./component/team";
 
 const AboutUsPage = () => {
-  // const { isMobile } = useScreenResolution();
 
   return (
     <>
       <AboutPage />
-      {/* <BookFlipComponent /> */}
-      {/* <Testimonials /> */}
       <CarouselWithModal />
       
       <div
@@ -26,18 +17,6 @@ const AboutUsPage = () => {
           background: "#fff",
         }}
       >
-        {/* <Partner
-          title={
-            isMobile
-              ? "Collaborative Partnerships Worldwide"
-              : "Our Global Partners"
-          }
-          description={
-            isMobile
-              ? "Our Collaborative Partnerships worldwide, prestigious institutions who believe in, and share our Vision with us."
-              : "Our study abroad, online college, and scholarship platform offers a wide range of benefits to students looking to achieve their academic and career goals"
-          }
-        /> */}
       </div>
     </>
   );
